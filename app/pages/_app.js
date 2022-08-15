75a3aeecf75d85dcd9da6aee40deb9be0f6b9abf
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-08-15 19:38:38 +0100

    config

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -7,25 +7,15 @@ import { useState } from "react";
 function MyApp({ Component, pageProps }) {
   const [isConnected, setIsConnected] = useState(false);
   const [currentAccount, setCurrentAccount] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const [userHasImpact, setUerHasImpact] = useState(false);
-  const [projects, setProjects] = useState([]);
-
   return (
     <AppContext.Provider
       value={{
         state: {
           isConnected,
           currentAccount,
-          accessToken,
-          userHasImpact,
-          projects,
         },
         setIsConnected,
         setCurrentAccount,
-        setAccessToken,
-        setUerHasImpact,
-        setProjects,
       }}
     >
       <Component {...pageProps} />
