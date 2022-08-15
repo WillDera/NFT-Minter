0033531c83bcb2662531c2f8cd02e7fa92e9e878
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-08-15 20:19:12 +0100

    redploy and verify

--- contracts/hepier.sol
+++ contracts/hepier.sol
@@ -11,26 +11,34 @@ contract Hepier is ERC1155, Ownable, ERC1155Burnable {
 
     Counters.Counter private _tokenIdCounter;
     string public name = "Hepier Collection";
+    address public operatorAddress;
 
     mapping(uint256 => string) public tokenURI;
 
+    modifier onlyOperator() {
+        require(msg.sender == operatorAddress, "Not operator");
+        _;
+    }
+
+    event NewOperatorAddress(address operator);
+
     constructor() ERC1155("HEP") {}
 
-    function setURI(string memory newuri) public onlyOwner {
+    function setURI(string memory newuri) public onlyOperator {
         uint256 tokenId = _tokenIdCounter.current();
         require(bytes(tokenURI[tokenId]).length == 0, "URI already set");
         tokenURI[tokenId] = newuri;
         _tokenIdCounter.increment();
     }
 
-    function mint(uint256 amount) public onlyOwner {
+    function mint(uint256 amount) public onlyOperator {
         uint256 tokenId = _tokenIdCounter.current();
         _mint(address(msg.sender), tokenId, amount, "");
     }
 
     function mintBatch(uint256[] memory ids, uint256[] memory amounts)
         public
-        onlyOwner
+        onlyOperator
     {
         _mintBatch(address(msg.sender), ids, amounts, "");
     }
@@ -39,10 +47,18 @@ contract Hepier is ERC1155, Ownable, ERC1155Burnable {
         return tokenURI[_id];
     }
 
-    function transfer(address to, uint256 amount) public onlyOwner {
+    function transfer(address to, uint256 amount) public onlyOperator {
         address from = address(msg.sender);
         uint256 tokenId = _tokenIdCounter.current();
 
         safeTransferFrom(from, to, tokenId, amount, "");
+    }
+
+    function setOperator(address _operatorAddress) public onlyOwner {
+        require(_operatorAddress != address(0), "Cannot be zero address");
+
+        operatorAddress = _operatorAddress;
+
+        emit NewOperatorAddress(_operatorAddress);
     }
 }
